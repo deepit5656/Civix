cc63d63e447f0f6321b8fecbb427ed9e17fa8d52
dhruv <dhruv@example.com>
2026-09-10 14:58:32 +0530

    Update mongo.js and index.jsx

--- backend/config/mongo.js
+++ backend/config/mongo.js
@@ -1,5 +1,4 @@
-const dns = require('dns');
-dns.setServers(['8.8.8.8', '1.1.1.1']);
+
 
 const mongoose = require('mongoose');
 require('dotenv').config();
--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import { ClerkProvider } from "@clerk/clerk-react";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
 
-const clerkPubKey = "pk_test_ZGlyZWN0LXNwb25nZS04NC5jbGVyay5hY2NvdW50cy5kZXYk";
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
 
 if (!clerkPubKey) {
   throw new Error("Missing Clerk publishable key. Check your .env file.");
